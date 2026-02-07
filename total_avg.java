3a24624e11ac304a5209e45ae268d7a679929e6f
Kaveri123319 <daddykaavi@gmail.com>
2026-02-07 11:35:04 +0530

    Add files via upload

---
+++ total_avg.java
@@ -0,0 +1,11 @@
+public class total_avg{
+    public static void main(String args[]){
+    int CNS=90;
+    int SPM=86;
+    int ML=92;
+    int total=CNS+SPM+ML;
+    int avg=total/2;
+    System.out.println(total);
+    System.out.println(avg);
+}
+}
