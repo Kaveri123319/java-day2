d055153182e8c4259a3e5f2b6e4076d1aeb25b4c
Kaveri123319 <daddykaavi@gmail.com>
2026-02-07 16:10:38 +0530

    Add files via upload

---
+++ swap_using_arithmetic.java
@@ -0,0 +1,12 @@
+public class swap_using_arithmetic {
+    public static void main(String args[])
+    {
+        int a=2;
+        int b=3;
+         a=a+b;
+            b=a-b;
+            a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
