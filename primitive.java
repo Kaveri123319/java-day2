136697d852cb2d14384bdca9e8495dd3f377f6c9
Kaveri123319 <daddykaavi@gmail.com>
2026-02-07 10:45:17 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,24 @@
+public class primitive {
+    public static void main(String []args)
+    {
+        byte b=120;
+        short a=159;
+        int c=10;
+        long l=1234;
+        float f=10.5f;
+        double d=20.234;
+        char ch='k';
+        char cha='@';
+        boolean value=true;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(cha);
+        System.out.println(b);
+        System.out.println(value);
+    }
+}
