db8bd07e7fa2c0c60ea9c009f6c6284c45934427
Kaveri123319 <daddykaavi@gmail.com>
2026-02-07 11:05:51 +0530

    Add files via upload

---
+++ primitive2.java
@@ -0,0 +1,23 @@
+public class primitive2 {
+    public static void main(String args[])
+    {
+        int age=20;
+        String name="Kaveri";
+        char grade='A';
+        int marks=90;
+        boolean status=true;
+        byte a=123;
+        short b=120;
+        float c=3.14f;
+        double d=20.45;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(marks);
+        System.out.println(status);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
